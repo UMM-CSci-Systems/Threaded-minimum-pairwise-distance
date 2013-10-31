dc0f7e575a8f07bd788356fb177df8d5d7ffe8fa
Nic McPhee <mcphee@morris.umn.edu>
2013-10-30 20:44:35 -0500

    Finished up some plausible tests for the serial minimum pairwise distance class.

--- test/mpd/SerialMinimumPairwiseDistanceTest.java
+++ test/mpd/SerialMinimumPairwiseDistanceTest.java
@@ -15,10 +15,30 @@ public class SerialMinimumPairwiseDistanceTest {
 	}
 
 	@Test
-	public void testEmpty() {
+	public void testEmptyArray() {
 		int[] values = new int[0];
 		int minimum = mpd.minimumPairwiseDistance(values);
 		assertEquals(Integer.MAX_VALUE, minimum);
 	}
+	
+	@Test
+	public void testSmallArrayWithDuplicates() {
+		int[] values = new int[] { 3, 2, 0, 5, 8, 9, 6, 3, 2, 0 };
+		int minimum = mpd.minimumPairwiseDistance(values);
+		assertEquals(0, minimum);
+	}
+	
+	@Test
+	public void testSmallSequence() {
+		int[] values = new int[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+		int minimum = mpd.minimumPairwiseDistance(values);
+		assertEquals(1, minimum);
+	}
 
+	@Test
+	public void testSmallRandomArray() {
+		int[] values = new int[] { 742428, 320304, 479193, 412922, 567421, 442073, 425546, 393948, 505578, 660888 };
+		int minimum = mpd.minimumPairwiseDistance(values);
+		assertEquals(12624, minimum);
+	}
 }
