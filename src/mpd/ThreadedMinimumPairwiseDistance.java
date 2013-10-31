9fafa7d7e9933040c63d37d69f2f40e07727a695
Nic McPhee <mcphee@morris.umn.edu>
2013-10-30 21:08:20 -0500

    Stubbed out Threaded Minimum Pairwise Distance.
    Created the tests for the threaded version (by copying the tests for the
    serial version) and stubbed out the necessary class to get things to
    compile.

---
+++ src/mpd/ThreadedMinimumPairwiseDistance.java
@@ -0,0 +1,10 @@
+package mpd;
+
+public class ThreadedMinimumPairwiseDistance implements MinimumPairwiseDistance {
+
+	@Override
+	public int minimumPairwiseDistance(int[] values) {
+		throw new UnsupportedOperationException();
+	}
+
+}
