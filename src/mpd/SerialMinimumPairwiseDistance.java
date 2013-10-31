8390a2683c740ee737530e599be640db07958745
Nic McPhee <mcphee@morris.umn.edu>
2013-10-31 00:19:25 -0500

    Add the solution to the simple serial case since they don't really need to do that on their own.

--- src/mpd/SerialMinimumPairwiseDistance.java
+++ src/mpd/SerialMinimumPairwiseDistance.java
@@ -4,7 +4,16 @@ public class SerialMinimumPairwiseDistance implements MinimumPairwiseDistance {
 
     @Override
     public int minimumPairwiseDistance(int[] values) {
-        throw new UnsupportedOperationException();
+        int result = Integer.MAX_VALUE;
+        for (int i = 0; i < values.length; ++i) {
+            for (int j = i + 1; j < values.length; ++j) {
+                int diff = Math.abs(values[i] - values[j]);
+                if (diff < result) {
+                    result = diff;
+                }
+            }
+        }
+        return result;
     }
 
 }
