e19ad4fe01eb656ed2f75354da94dfbc935bd0e1
Nic McPhee <mcphee@morris.umn.edu>
2013-10-31 00:53:03 -0500

    Modified the loop bounds in the serial solution to better match the diagrams in the write up.

--- src/mpd/SerialMinimumPairwiseDistance.java
+++ src/mpd/SerialMinimumPairwiseDistance.java
@@ -6,7 +6,8 @@ public class SerialMinimumPairwiseDistance implements MinimumPairwiseDistance {
     public int minimumPairwiseDistance(int[] values) {
         int result = Integer.MAX_VALUE;
         for (int i = 0; i < values.length; ++i) {
-            for (int j = i + 1; j < values.length; ++j) {
+            for (int j = 0; j < i; ++j) {
+                // Gives us all the pairs (i, j) where 0 <= j < i < values.length
                 int diff = Math.abs(values[i] - values[j]);
                 if (diff < result) {
                     result = diff;
